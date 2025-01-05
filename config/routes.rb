fafb1de972ba20499daf365d2f4febe2e35f725f
gotoshota0116 <lgy.w30@gmail.com>
2025-01-05 21:51:12 +0900

    add:minimagickを使用して、画像生成完了,ブラウザで確認

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+   get 'images/ogp.png', to: 'images#ogp', as: 'images_ogp'
+   # asをつかうことで、images_ogp_pathのヘルパーを使うことができる。resourceなどは自動でヘルパーを生成してくれる。
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
