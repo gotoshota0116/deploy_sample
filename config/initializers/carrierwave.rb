890c381cbfabd02b7ce5da087279a7d3a2bce8ab
gotoshota0116 <lgy.w30@gmail.com>
2025-01-11 23:53:14 +0900

    improve:ACL設定加筆

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,21 +1,18 @@
-require 'carrierwave/storage/fog'
-# require 'carrierwave/orm/activerecord' # ActiveRecordとの統合が必要な場合
+require 'carrierwave/storage/fog' # Fogを使用したAWS S3などのクラウドストレージに対応するための設定を読み込み
+require 'carrierwave/orm/activerecord' # ActiveRecordとCarrierWaveの統合が必要な場合に使用（例: マウントしたカラムを扱う場合）
 
 CarrierWave.configure do |config|
-	if Rails.env.production?
+    # 本番環境でAWS S3を使用してファイルを保存する設定
     # config.storage :fog
-    	# config.fog_provider = 'fog/aws'
-    	config.fog_directory  = ENV['AWS_S3_BUCKET'] # envファイルで設定
-    	config.fog_credentials = {
-    	  provider: 'AWS',
-    	  aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
-    	  aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
-    	  region: ENV['AWS_REGION'],   # アジアパシフィック(東京)を選択した場合
-    	}
-	else
-	  # 開発環境/テスト環境: ローカルストレージを使用
-      config.storage = :file
-      config.enable_processing = Rails.env.development? # 開発時のみ画像処理を有効化
-  	end
+    # config.fog_provider = 'fog/aws' # FogをAWSプロバイダとして設定
+    config.fog_directory  = ENV['AWS_S3_BUCKET'] # S3のバケット名を環境変数から取得
+    config.fog_credentials = {
+      provider: 'AWS', # 使用するプロバイダをAWSに設定
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数に保存されたAWSのアクセスキーID
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数に保存されたAWSのシークレットキー
+      region: ENV['AWS_REGION'], # 使用するAWSリージョン（例: 東京の場合はap-northeast-1）
+    }
+    #config.fog_public     = false # アップロードされたファイルを非公開に設定（署名付きURLを使用する場合に必要）
+    config.fog_attributes = {'x-amz-acl' => nil} # 明示的にACLを無効化（バケットポリシーで管理）
+	#config.storage = :file # ファイルをローカルストレージに保存　アップローダーに記載している！
 end
-
