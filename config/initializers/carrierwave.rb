9ea127eec0c98a703e2d0246ab50c2790cd2abe6
gotoshota0116 <lgy.w30@gmail.com>
2025-01-12 00:08:10 +0900

    improve:carrierwave.rb編集

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ require 'carrierwave/orm/activerecord' # ActiveRecordとCarrierWaveの統合が�
 
 CarrierWave.configure do |config|
     # 本番環境でAWS S3を使用してファイルを保存する設定
-    # config.storage :fog
+     config.storage :fog
     # config.fog_provider = 'fog/aws' # FogをAWSプロバイダとして設定
     config.fog_directory  = ENV['AWS_S3_BUCKET'] # S3のバケット名を環境変数から取得
     config.fog_credentials = {
@@ -12,7 +12,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数に保存されたAWSのシークレットキー
       region: ENV['AWS_REGION'], # 使用するAWSリージョン（例: 東京の場合はap-northeast-1）
     }
-    #config.fog_public     = false # アップロードされたファイルを非公開に設定（署名付きURLを使用する場合に必要）
+    config.fog_public     = false # アップロードされたファイルを非公開に設定（署名付きURLを使用する場合に必要）
     config.fog_attributes = {'x-amz-acl' => nil} # 明示的にACLを無効化（バケットポリシーで管理）
 	#config.storage = :file # ファイルをローカルストレージに保存　アップローダーに記載している！
 end
